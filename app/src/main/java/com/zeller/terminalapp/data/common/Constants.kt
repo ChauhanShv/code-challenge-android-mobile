318a160c0b3e8ca5487adbf1f54ec9e5d5fc58c8
s.chauvan <s.chauvan@itrun.dev>
2025-03-06 11:59:24 +0530

    feat: constants used in data layer for error and min withdrawn balance allowed

---
+++ app/src/main/java/com/zeller/terminalapp/data/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.zeller.terminalapp.data.common
+
+// these can be moved to some constant file
+const val MIN_NOT_SUPPORTED_AMOUNT = 0.1F
+const val MIN_WITHDRAW_ERR_MESSAGE = "Minimum withdraw amount should be more than 1 cents."
+const val NOT_ENOUGH_BALANCE = "Not enough balance"
